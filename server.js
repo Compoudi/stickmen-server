5a53f20906f473e1cfa4c6a171b68c9fa0c16d35
Compoudi <ghislairo@gmail.com>
2025-11-07 08:52:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ function createRoom() {
   const id = "room-" + Math.random().toString(36).substr(2, 6);
   const engine = Matter.Engine.create();
   const world = engine.world;
-  world.gravity.y = 1.2;
+  world.gravity.y = 0.6;
 
   // Sol
   const ground = Matter.Bodies.rectangle(400, 580, 800, 40, {
@@ -159,5 +159,6 @@ wss.on("connection", (ws) => {
     }
   });
 });
+
 
 
